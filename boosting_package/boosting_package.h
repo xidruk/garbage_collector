7525aa0b20389cb3a4036feac641c8d1c1e09c94
Xidruk <186780264+xidruk@users.noreply.github.com>
2025-03-24 15:12:30 -0700

    Create boosting_package.h

---
+++ boosting_package/boosting_package.h
@@ -0,0 +1,12 @@
+#ifndef BOOSTING_PACKAGE_H
+#define BOOSTING_PACKAGE_H
+
+//---> includes 
+#include <stdio.h>
+
+//---> functions 
+
+size_t	string_size(const char *ptr);
+void memory_copy(void *target,const void *src, size_t size);
+
+#endif
